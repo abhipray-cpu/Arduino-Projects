c8b2df9e8a61350767303a6933b48ff2183820b1
Abhipray Dumka <dumkaabhipray@gmail.com>
2023-06-12 23:28:33 +0530

    Update transmitter_cpp.ino

--- gesture controlled bot/transmitter_cpp/transmitter_cpp.ino
+++ gesture controlled bot/transmitter_cpp/transmitter_cpp.ino
@@ -2,9 +2,11 @@
 #include<RF24.h>
 #include<nRF24L01.h>
 #include <Wire.h>
-#include <MPU6050.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
-MPU6050 mpu;
+
+Adafruit_MPU6050 mpu;
 //create an RF24 object
 RF24 radio(9, 10);  // CE, CSN
 
@@ -23,15 +25,71 @@ void setup()
   Serial.begin(9600);
 
   Serial.println("Initialize MPU6050");
-  radio.
 
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  while(!mpu.begin())
   {
     Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
     delay(500);
   }
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
 
-  
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+  // this is for nrf library
   radio.begin();
 
   
@@ -45,12 +103,15 @@ void setup()
 
 void loop()
 {
-  // Read normalized values 
-  Vector normAccel = mpu.readNormalizeAccel();
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  
 
   // Calculate Pitch & Roll
-  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
-  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
+
+  int pitch = -(atan2(a.acceleration.x, sqrt(a.acceleration.x*a.acceleration.y + a.acceleration.z*a.acceleration.z))*180.0)/M_PI;
+  int roll = (atan2(a.acceleration.y, a.acceleration.z)*180.0)/M_PI;
+
   
 
  //Output
